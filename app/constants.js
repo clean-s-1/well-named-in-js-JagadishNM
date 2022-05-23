c83eb07dc7a262abde8e23df41d185303d0116e4
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-23 16:32:16 +0530

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -5,6 +5,3 @@ const MINOR_COLOR_NAMES = [
 	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
 ];
 module.exports = {MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES}
-
-// export const MAJOR_COLORS_COUNT = MAJOR_COLOR_NAMES.length;
-// export const MINOR_COLORS_COUNT = MINOR_COLOR_NAMES.length;
