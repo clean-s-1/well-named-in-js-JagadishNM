d695e0005512e885819053b009c35298d37ffeeb
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-23 15:04:00 +0530

    Add files via upload

---
+++ app/ColorPair.js
@@ -0,0 +1,9 @@
+export class ColorPair {
+    constructor() {
+        this.majorColor;
+        this.minorColor;
+    }
+    toString() {
+        return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
+    }
+}
---
+++ app/constants.js
@@ -0,0 +1,10 @@
+export const MAJOR_COLOR_NAMES = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+export const MINOR_COLOR_NAMES = [
+	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+
+// export const MAJOR_COLORS_COUNT = MAJOR_COLOR_NAMES.length;
+// export const MINOR_COLORS_COUNT = MINOR_COLOR_NAMES.length;
