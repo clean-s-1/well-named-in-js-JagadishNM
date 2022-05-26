adcf187755d0a0984416fec97b1e91bf60459939
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-26 22:23:41 +0530

    Update colorCodeConversion.js

--- app/colorCodeConversion.js
+++ app/colorCodeConversion.js
@@ -1,13 +1,21 @@
 const ColorPair = require("../app/ColorPair");
-const {MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES} = require("../app/constants");
+const { MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES } = require("../app/constants");
 
 module.exports = {
-    getColorFromPairNumber: function(pairNumber) {
-        let minorSize = MAJOR_COLOR_NAMES.length;
+  getColorIndex: function (colorNames, color) {
+    for (let i = 0; i < colorNames.length; i++) {
+      if (colorNames[i] == color) {
+        return i;
+      }
+    }
+    return -1;
+  },
+  getColorFromPairNumber: function (pairNumber) {
+    let minorSize = MAJOR_COLOR_NAMES.length;
     let majorSize = MINOR_COLOR_NAMES.length;
 
     if (pairNumber < 1 || pairNumber > minorSize * majorSize) {
-        throw `Argument PairNumber:${pairNumber} is outside the allowed range`
+      throw `Argument PairNumber:${pairNumber} is outside the allowed range`;
     }
     let zeroBasedPairNumber = pairNumber - 1;
     let majorIndex = parseInt(zeroBasedPairNumber / minorSize);
@@ -16,30 +24,16 @@ module.exports = {
     pair.majorColor = MAJOR_COLOR_NAMES[majorIndex];
     pair.minorColor = MINOR_COLOR_NAMES[minorIndex];
     return pair;
-    },
+  },
 
-    getPairNumberFromColor: function(pair) {
-        let majorIndex = -1;
-    for (let i = 0; i < MAJOR_COLOR_NAMES.length; i++) {
-        if (MAJOR_COLOR_NAMES[i] == pair.majorColor) {
-            majorIndex = i;
-            break;
-        }
-    }
-
-    let minorIndex = -1;
-    for (let i = 0; i < MINOR_COLOR_NAMES.length; i++) {
-        if (MINOR_COLOR_NAMES[i] == pair.minorColor) {
-            minorIndex = i;
-            break;
-        }
-    }
+  getPairNumberFromColor: function (pair) {
+    let majorIndex = getColorIndex(MAJOR_COLOR_NAMES, pair.majorColor);
+    let minorIndex = getColorIndex(MINOR_COLOR_NAMES, pair.minorColor);
 
     if (majorIndex == -1 || minorIndex == -1) {
-        throw `Unknown Colors:${pair.toString()}`;
+      throw `Unknown Colors:${pair.toString()}`;
     }
 
-    return (majorIndex * MINOR_COLOR_NAMES.length) + (minorIndex + 1);
-    }
-
-}
+    return majorIndex * MINOR_COLOR_NAMES.length + (minorIndex + 1);
+  },
+};
